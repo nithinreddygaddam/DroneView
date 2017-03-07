d990a42522542a26bd7514d4d19727fe37258607
Nithin Reddy <nithinreddygaddam@gmail.com>
2017-03-07 18:18:00 -0500

    Initial Commit

---
+++ Drone View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drone View
+//
+//  Created by Nithin Reddy Gaddam on 3/7/17.
+//  Copyright © 2017 Nithin Reddy Gaddam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
